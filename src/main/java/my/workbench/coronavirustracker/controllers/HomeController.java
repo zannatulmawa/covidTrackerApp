f4220b2f27a8f1af670e454173ea9c895c19d634
Zannat <zannatulmawa62@gmail.com>
2022-05-05 13:07:12 -0400

    thymeleaf model added for using html

--- src/main/java/my/workbench/coronavirustracker/controllers/HomeController.java
+++ src/main/java/my/workbench/coronavirustracker/controllers/HomeController.java
@@ -1,13 +1,18 @@
 package my.workbench.coronavirustracker.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller // A spring controller is using for showing the page into a html format to a url.
 public class HomeController {
 
-    @GetMapping("/")
-    public String home(){ //It will render/know the html page as we have added thymeleaf in the dependency
+    @GetMapping("/") //this will take us to the root directory
+    public String home(Model modelObj){
+        //It will render/know the html page as we have added thymeleaf in the dependency
+        //Model is imported from spring framework ui, so that we can design the html within a model
+        //we need to create a modelObj for that
+        modelObj.addAttribute("testName", "Test");
         return "home";
     }
 }
