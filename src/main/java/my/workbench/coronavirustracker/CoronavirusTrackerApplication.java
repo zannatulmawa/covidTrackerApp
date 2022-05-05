58fdb5d3c10dbf98f1a3fe593adcc23b2afee58f
Zannat <zannatulmawa62@gmail.com>
2022-05-05 11:30:16 -0400

    run the covid tracker app for every updated time using the scheduled cron anootation

--- src/main/java/my/workbench/coronavirustracker/CoronavirusTrackerApplication.java
+++ src/main/java/my/workbench/coronavirustracker/CoronavirusTrackerApplication.java
@@ -2,8 +2,10 @@ package my.workbench.coronavirustracker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling //tell spring to run the scheduled method according to it's requirement
 public class CoronavirusTrackerApplication {
 
 	public static void main(String[] args) {
--- src/main/java/my/workbench/coronavirustracker/services/CoronaVirusDataService.java
+++ src/main/java/my/workbench/coronavirustracker/services/CoronaVirusDataService.java
@@ -1,5 +1,6 @@
 package my.workbench.coronavirustracker.services;
 
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Service;
 import org.apache.commons.csv.CSVRecord;
 import org.apache.commons.csv.CSVFormat;
@@ -17,6 +18,7 @@ public class CoronaVirusDataService {
     private static String virusDataUrl = "https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv";
 
     @PostConstruct //In order to make a post request through spring
+    @Scheduled(cron = "* * * * * *") //calling spring to execute the method every second
     public void fetchVirusData() throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
         HttpRequest request = HttpRequest.newBuilder().uri(URI.create(virusDataUrl)).build(); //creating the request
@@ -24,10 +26,13 @@ public class CoronaVirusDataService {
         System.out.println(httpResponse.body());
 
 
+        //In order to parse the csv file, we take this code from an open source library
         StringReader csvBodyReader = new StringReader(httpResponse.body());
+        //import the string reader, creating csv body reader object
+        //to convert the httpResponse into a stringReader so that we can parse the csv file here
         Iterable<CSVRecord> records = CSVFormat.DEFAULT.withFirstRecordAsHeader().parse(csvBodyReader);
-        for (CSVRecord record : records) {
-            String state = record.get("Province/State");
+        for (CSVRecord record : records) { //looping through the record
+            String state = record.get("Province/State"); //pulling out one column from here
             System.out.println(state);
 
         }
